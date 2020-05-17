63827172e11a7e8c50a7513e48f440a4f432ff58
Jensen Koch <jtkoch12@gmail.com>
2020-05-17 10:19:11 -0500

    completely refactored project

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -3,30 +3,73 @@ module.exports = {
   fail,
   repair,
   get,
-}
+};
 
 function succeed(item) {
-  if (item.enhancement < 20) {
-    return { ...item, enhancement: item.enhancement + 1 }
+
+  const { enhancement } = item;
+
+  if (item.enhancement >= 0 && item.enhancement < 20) {
+    const newEnhancement = enhancement + 1;
+    return { ...item, enhancement: newEnhancement };
+  } else if (item.enhancement === 20) {
+    return { ...item, enhancement: item.enhancement };
   } else {
-    return item
+    return "enhancement level passed should be less than 20";
   }
+
 }
 
 function fail(item) {
-  if (item.enhancement < 15) {
-    return { ...item, durability: item.durability - 5 }
-  } else if (item.enhancement >= 15) {
-    return { ...item, durability: item.durability - 10 }
-  } else if (item.enhancement > 16) {
-    return { ...item, durability: item.enhancement - 1 }
+
+  let newDurability = item.durability;
+  let newEnhancement = item.enhancement;
+  if (item.enhancement >= 0 && item.enhancement < 15) {
+    newDurability = item.durability - 5;
+    return { ...item, durability: newDurability };
+  } else if (item.enhancement >= 15 && item.enhancement <= 20) {
+    if (item.enhancement <= 16) {
+      newDurability = item.durability - 10;
+      return { ...item, durability: newDurability };
+    } else {
+      newDurability = item.durability - 10;
+      newEnhancement = item.enhancement - 1;
+      return {
+        ...item,
+        durability: newDurability,
+        enhancement: newEnhancement,
+      };
+    }
+  } else {
+    return "please correct item parameters passed in";
   }
 }
 
+
 function repair(item) {
-  return { ...item, durability: 100 }
+
+  if (
+    item.durability <= 100 &&
+    item.durability >= 0 &&
+    item.enhancement >= 0 &&
+    item.enhancement <= 20
+  ) {
+    return { ...item, durability: 100 };
+  } else {
+    return "item with wrong parameters passed";
+  }
 }
 
 function get(item) {
-  return { ...item }
+
+  let newName = item.name;
+  if (item.enhancement > 0 && item.enhancement <= 20) {
+    newName = `[+${item.enhancement}] ${item.name}`;
+    console.log("new name: ", newName);
+    return { ...item, name: newName };
+  } else if (item.enhancement === 0) {
+    return { ...item };
+  } else {
+    return "please pass item with correct parameters";
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,28 +1,134 @@
-const enhancer = require("./enhancer.js")
+const enhancer = require("./enhancer.js");
+// test away!
 
-it("should run the tests", function() {
-   expect(true).toBe(true)
+describe("a repair(item) method that accepts an item object and returns a new item with the durability restored to 100", () => {
+  it("should accept an object and return 100 durability", () => {
+    expect(
+      enhancer.repair({
+        name: "Item1",
+        durability: 0,
+        enhancement: 18,
+      })
+    ).toEqual({
+      name: "Item1",
+      enhancement: 18,
+      durability: 100,
+    });
+  });
+
+  it("should accept an object, ensures the returned value is 100, and not anythign else", () => {
+    expect(
+      enhancer.repair({
+        name: "Item1",
+        durability: 0,
+        enhancement: 18,
+      })
+    ).not.toEqual({
+      name: "Item1",
+      enhancement: 18,
+      durability: 98,
+    });
+  });
+});
+
+describe("a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success", () => {
+  it("should return item's enhancement increases by 1.", () => {
+    expect(
+      enhancer.succeed({
+        name: "Item2",
+        durability: 0,
+        enhancement: 1,
+      })
+    ).toEqual({
+      name: "Item2",
+      durability: 0,
+      enhancement: 2,
+    });
+  });
+
+  it("should return ehancement level 20 if passed in 20", () => {
+    expect(
+      enhancer.succeed({
+        name: "Item2",
+        durability: 0,
+        enhancement: 20,
+      })
+    ).toEqual({
+      name: "Item2",
+      durability: 0,
+      enhancement: 20,
+    });
+
+    expect(
+      enhancer.succeed({
+        name: "Item2",
+        durability: 0,
+        enhancement: 20,
+      })
+    ).not.toEqual({
+      name: "Item2",
+      durability: 0,
+      enhancement: 21,
+    });
+  });
+});
+
+describe("a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.", () => {
+  it("should decrease durability by 5 if enhancement is less than 15", () => {
+    expect(
+      enhancer.fail({
+        name: "item3",
+        durability: 90,
+        enhancement: 10,
+      })
+    ).toEqual({
+      name: "item3",
+      durability: 85,
+      enhancement: 10,
+    });
+  });
+
+  it("should decrease durability by 10 if enhancement is 15 or more", () => {
+    expect(
+      enhancer.fail({
+        name: "item3",
+        durability: 80,
+        enhancement: 16,
+      })
+    ).toEqual({
+      name: "item3",
+      durability: 70,
+      enhancement: 16,
+    });
+  });
 })
 
-describe("enhancer", () => {
-  describe(".succeed()", () => {
-    it.todo("should add 1 to item.enhancement property if <20")
-    it.todo("should return item if item.enhancement is 20")
-    it.todo("should throw an error if the arg is not an object")
-    it.todo("should throw an error if item.enhancement is >20")
-  })
+describe("Add a get() method to the enhancer object that takes an item and returns a new item with the name property modified according to the following rules:", () => {
+  it("if the enhancement level is greater than 0, change the name to include the enhancement level, preceded by a plus sign ( + ), between square brackets before the item's name. Example: the name of a Iron Sword enhanced to 7 would be [+7] Iron Sword ", () => {
+    expect(
+      enhancer.get({
+        name: "item1",
+        durability: 10,
+        enhancement: 7,
+      })
+    ).toEqual({
+      name: "[+7] item1",
+      durability: 10,
+      enhancement: 7,
+    });
+  });
 
-  describe(".fail()", () => {
-    it.todo("should subtract 5 from durability property if item.enhancement < 15")
-    it.todo("should subtract 10 from durability property if item.enhancement >= 15")
-    it.todo("should subtract 1 from enhancement property if item.enhancement > 16")
-    it.todo("should throw an error if the arg is not an object")
-    it.todo("should throw an error if item.enhancement is > 20")
-    it.todo("should throw an error if item.durability is > 100")
-  })
-
-  describe(".repair()", () => {
-    it.todo("should set the item.durability property to 100")
-    it.todo("should throw an error if the arg is not an object")
-  })
-})
+  it("if the enhancement level is 0, the the name is not modified.", () => {
+    expect(
+      enhancer.get({
+        name: "item1",
+        durability: 10,
+        enhancement: 0,
+      })
+    ).toEqual({
+      name: "item1",
+      durability: 10,
+      enhancement: 0,
+    });
+  });
+});
